4b4543146295f43bf60901f0c9eb44d3cd9639f6
vandatnguyen919 <vandatnguyen919@gmail.com>
2025-02-15 10:14:04 +0700

    Open OTP endpoint

--- src/main/java/com/pregnancy/edu/security/SecurityConfiguration.java
+++ src/main/java/com/pregnancy/edu/security/SecurityConfiguration.java
@@ -71,6 +71,7 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
                         .requestMatchers(HttpMethod.POST, this.baseUrl + "/auth/login").authenticated() // Protect this endpoint
                         .requestMatchers(HttpMethod.POST, this.baseUrl + "/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.POST, this.baseUrl + "/otp/**").permitAll()
                         .requestMatchers(HttpMethod.GET, this.baseUrl + "/users/**").hasAuthority("ROLE_admin") // Protect this endpoint
                         .requestMatchers(HttpMethod.POST, this.baseUrl + "/users").hasAuthority("ROLE_admin") // Protect this endpoint
                         .requestMatchers(HttpMethod.PUT, this.baseUrl + "/users/**").hasAuthority("ROLE_admin") // Protect this endpoint

97ca71173dbd2e7e352497baf6bb669be310fa6d
vandatnguyen919 <vandatnguyen919@gmail.com>
2025-03-25 19:57:19 +0700

    Add metric data to standard

--- src/main/java/com/pregnancy/edu/fetusinfo/standard/converter/StandardToStandardDtoConverter.java
+++ src/main/java/com/pregnancy/edu/fetusinfo/standard/converter/StandardToStandardDtoConverter.java
@@ -1,5 +1,6 @@
 package com.pregnancy.edu.fetusinfo.standard.converter;
 
+import com.pregnancy.edu.fetusinfo.metric.converter.MetricToMetricDtoConverter;
 import com.pregnancy.edu.fetusinfo.standard.Standard;
 import com.pregnancy.edu.fetusinfo.standard.dto.StandardDto;
 import org.springframework.core.convert.converter.Converter;
@@ -7,11 +8,19 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class StandardToStandardDtoConverter implements Converter<Standard, StandardDto> {
+
+    private final MetricToMetricDtoConverter metricToMetricDtoConverter;
+
+    public StandardToStandardDtoConverter(MetricToMetricDtoConverter metricToMetricDtoConverter) {
+        this.metricToMetricDtoConverter = metricToMetricDtoConverter;
+    }
+
     @Override
     public StandardDto convert(Standard source) {
         return new StandardDto(
                 source.getId(),
                 source.getMetric().getId(),
+                metricToMetricDtoConverter.convert(source.getMetric()),
                 source.getWeek(),
                 source.getMin(),
                 source.getMax()
--- src/main/java/com/pregnancy/edu/fetusinfo/standard/dto/StandardDto.java
+++ src/main/java/com/pregnancy/edu/fetusinfo/standard/dto/StandardDto.java
@@ -1,11 +1,13 @@
 package com.pregnancy.edu.fetusinfo.standard.dto;
 
+import com.pregnancy.edu.fetusinfo.metric.dto.MetricDto;
 import jakarta.validation.constraints.NotNull;
 
 public record StandardDto(
         Long id,
         @NotNull(message = "Metric ID is required")
         Long metricId,
+        MetricDto metric,
         @NotNull(message = "Week is required")
         Integer week,
         @NotNull(message = "Minimum value is required")
